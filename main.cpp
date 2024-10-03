afd2fc79bb82d3199d33100ed701ddb2f18c17f9
Simone-Gramuglia <simone.gramuglia@marconibari.edu.it>
2024-10-03 15:33:02 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,40 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
+   int N, medio, i, pro, deb, somma;
+   float media;
+   pro = 0;
+   deb = 0;
+   somma = 0;
+   media = 0;
+   string nome;
+   cout << "Quanti studenti ci sono in questa classe?" << endl;
+   cin >> N;
+   while (N<0) {
+    cout << "Non valgono valori negativi" << endl;
+    cin >> N;
+   } 
+   int stud[N];
+   for (i=1; i<=N; i++){
+    cout << "Qual è il tuo nome?" << endl;
     cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+    cout << "Qual è il tuo voto medio in informatica?" << endl;
+    cin >> medio;
+    somma = somma + medio;
+    if (medio<6) {
+        cout << nome << " prenderà il debito" << endl;
+        deb = deb+1;
+    }
+    else {
+        cout << nome << " non prenderà il debito" << endl;
+        pro = pro+1;
+    }
+   }
+   cout << "in totale " << pro << " studenti saranno promossi e " << deb << " saranno bocciati" << endl;
+   media = somma/N;
+   cout << "La media totale della classe è " << media; 
 }
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+
